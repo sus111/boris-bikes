cc9b7810635c26f5423598712dcc13f3efd746b3
Georgia Mills <georgee_123@hotmail.com>
2016-07-06 15:41:39 +0100

    added docking tests and methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
-  def release_bike
-    Bike.new
-  end
 
   def dock(bike)
+fail 'Docking station full' if @bike
     @bike = bike
+  end
+  
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
  attr_reader :bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,24 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it 'raises an error when there are no bikes available' do
+  expect {subject.release_bike}.to raise_error 'No bikes available'
+  end
+end
+
+describe '#dock'do
+it 'raises an error when full' do
+  subject.dock(Bike.new)
+  expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+end
+end 
 
 end
