83e2bd38f5b808d7b08eda3aced15941fc91c6db
sus111 <sgraham535@gmail.com>
2016-07-07 12:23:50 +0100

    error fixing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,15 +10,21 @@ class DockingStation
     end
 
     def dock(bike)
-        raise "Docking Station full" if @bikes.count >= 20
+        raise "Docking Station full" if self.full?
         @bikes.push(bike)
         self.show_last_bike
     end
+
 
     #attr_reader :bikes
 
     def show_last_bike
       @bikes.last
+    end
+
+
+    def full?
+      @bikes.count >= 20
     end
 
 end
